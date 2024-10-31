f38031fcc92dfcd0c1cbc603d3f56758eeffecf9
SeminaMargarita <m2411875@edu.misis.ru>
2024-10-31 15:49:47 +0300

    БИВТ 24-11 Сёмина Маргарита

--- Lab2/obj/Release/net8.0/Lab2.AssemblyInfo.cs
+++ Lab2/obj/Release/net8.0/Lab2.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab2")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e11a2654ccc2ced5c9482c0022897a3013bd4a58")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab2")]
